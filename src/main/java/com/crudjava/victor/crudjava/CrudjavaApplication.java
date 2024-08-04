25ed05e1e48b1e02fe4900951610979354d29bd1
victor benavente <templario0141@gmail.com>
2024-08-03 21:37:28 -0500

    Initial commit

---
+++ src/main/java/com/crudjava/victor/crudjava/CrudjavaApplication.java
@@ -0,0 +1,13 @@
+package com.crudjava.victor.crudjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudjavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crudjava/victor/crudjava/CrudjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crudjava.victor.crudjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
