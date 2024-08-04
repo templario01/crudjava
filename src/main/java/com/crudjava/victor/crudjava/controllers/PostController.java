ed47a52134e11ba08ce57fa88d08921087959d7e
victor benavente <templario0141@gmail.com>
2024-08-03 23:28:49 -0500

    feat: add delete endpoint

--- src/main/java/com/crudjava/victor/crudjava/controllers/PostController.java
+++ src/main/java/com/crudjava/victor/crudjava/controllers/PostController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.crudjava.victor.crudjava.dtos.CreatePostDto;
@@ -12,6 +13,8 @@ import com.crudjava.victor.crudjava.entities.Post;
 import com.crudjava.victor.crudjava.services.PostService;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -31,6 +34,12 @@ public class PostController {
                 postCreated.getContent());
 
         return ResponseEntity.status(HttpStatus.CREATED).body(post);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletePost(@PathVariable("id") Long id){
+        postService.deletePost(id);
     }
 
 }
--- src/main/java/com/crudjava/victor/crudjava/services/PostService.java
+++ src/main/java/com/crudjava/victor/crudjava/services/PostService.java
@@ -1,8 +1,12 @@
 package com.crudjava.victor.crudjava.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.crudjava.victor.crudjava.dtos.CreatePostDto;
 import com.crudjava.victor.crudjava.entities.Post;
@@ -20,6 +24,19 @@ public class PostService {
         post.setContent(request.getContent());
 
         return postRepository.save(post);
+    }
+
+    @Transactional
+    public void deletePost(Long id) {
+        Optional<Post> post = postRepository.findById(id);
+        if (!post.isPresent()) {
+            throw this.throwNotFoundException();
+        }
+        postRepository.deleteById(id);
+    }
+
+    private ResponseStatusException throwNotFoundException() {
+        return new ResponseStatusException(HttpStatus.NOT_FOUND);
     }
 
 }
